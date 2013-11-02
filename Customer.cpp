e52d04f117165b23d41aaee148039d40be8f051b
VitaliyKhotey <vitaly.khotey@gmail.com>
2013-11-02 17:11:48 +0200

    add class customer

---
+++ Customer.cpp
@@ -0,0 +1,9 @@
+//
+//  Customer.cpp
+//  sa
+//
+//  Created by Vitaly Khotey on 02.11.13.
+//  Copyright (c) 2013 Vitaly Khotey. All rights reserved.
+//
+
+#include "Customer.h"
---
+++ Customer.h
@@ -0,0 +1,14 @@
+//
+//  Customer.h
+//  sa
+//
+//  Created by Vitaly Khotey on 02.11.13.
+//  Copyright (c) 2013 Vitaly Khotey. All rights reserved.
+//
+
+#ifndef __sa__Customer__
+#define __sa__Customer__
+
+#include <iostream>
+
+#endif /* defined(__sa__Customer__) */
